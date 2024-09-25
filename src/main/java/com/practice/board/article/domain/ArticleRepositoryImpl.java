f7f54d9207541c517e846f58fba6595f879afb1f
101 <tmddnjs001133@dsm.hs.kr>
2024-09-25 23:37:09 +0900

    add: article repository implements class

--- src/main/java/com/practice/board/article/domain/ArticleRepositoryImpl.java
+++ src/main/java/com/practice/board/article/domain/ArticleRepositoryImpl.java
@@ -1,2 +1,31 @@
-package com.practice.board.article.domain;class ArticleRepositoryImpl {
+package com.practice.board.article.domain;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+class ArticleRepositoryImpl implements ArticleRepository {
+    private final ArticleJpaRepository repository;
+    private final ArticleMapper mapper;
+
+    @Override
+    public ArticleDto getById(final Long id) {
+        final var entity = repository.findById(id).orElseThrow();
+
+        return mapper.toDto(entity);
+    }
+
+    @Override
+    public ArticleDto save(final ArticleDto article) {
+        final var entity = mapper.toEntity(article);
+        final var savedEntity = repository.save(entity);
+
+        return mapper.toDto(savedEntity);
+    }
+
+    @Override
+    public void deleteById(final Long id) {
+        repository.deleteById(id);
+    }
 }
