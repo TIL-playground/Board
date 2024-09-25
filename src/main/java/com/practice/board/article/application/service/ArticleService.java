92989fa9da9db770b0dec23cb1019b78f0775859
101 <tmddnjs001133@dsm.hs.kr>
2024-09-25 23:37:23 +0900

    add: article service implements class

--- src/main/java/com/practice/board/article/application/service/ArticleService.java
+++ src/main/java/com/practice/board/article/application/service/ArticleService.java
@@ -1,2 +1,9 @@
-package com.practice.board.article.application.service;class ArticleService {
+package com.practice.board.article.application.service;
+
+import com.practice.board.article.domain.ArticleDto;
+
+public interface ArticleService {
+    ArticleDto save(ArticleDto dto);
+    ArticleDto get(Long id);
+    void delete(Long id);
 }
--- src/main/java/com/practice/board/article/application/service/ArticleServiceImpl.java
+++ src/main/java/com/practice/board/article/application/service/ArticleServiceImpl.java
@@ -1,2 +1,30 @@
-package com.practice.board.article.application.service;class ArticleServiceImpl {
+package com.practice.board.article.application.service;
+
+import com.practice.board.article.domain.ArticleDto;
+import com.practice.board.article.domain.ArticleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+class ArticleServiceImpl implements ArticleService {
+    private final ArticleRepository repository;
+
+    @Override
+    public ArticleDto save(final ArticleDto dto) {
+        return repository.save(dto);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public ArticleDto get(final Long id) {
+        return repository.getById(id);
+    }
+
+    @Override
+    public void delete(final Long id) {
+        repository.deleteById(id);
+    }
 }
