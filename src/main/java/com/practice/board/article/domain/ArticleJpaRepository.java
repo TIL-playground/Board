d3317c954ceb9c07a082dae1e955a5dd3d3c745e
101 <tmddnjs001133@dsm.hs.kr>
2024-09-25 23:36:51 +0900

    add: article jpa repository

--- src/main/java/com/practice/board/article/domain/ArticleJpaRepository.java
+++ src/main/java/com/practice/board/article/domain/ArticleJpaRepository.java
@@ -1,2 +1,6 @@
-package com.practice.board.article.domain;interface ArticleJpaRepository {
+package com.practice.board.article.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+interface ArticleJpaRepository extends JpaRepository<Article, Long> {
 }
