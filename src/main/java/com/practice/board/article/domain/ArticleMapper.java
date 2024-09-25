29ee5834ea9f57e47f515839ac95de09418f1b72
101 <tmddnjs001133@dsm.hs.kr>
2024-09-25 23:36:27 +0900

    add: article mapper class

--- src/main/java/com/practice/board/article/domain/ArticleMapper.java
+++ src/main/java/com/practice/board/article/domain/ArticleMapper.java
@@ -1,2 +1,11 @@
-package com.practice.board.article.domain;interface ArticleMapper {
+package com.practice.board.article.domain;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING, unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface ArticleMapper {
+    ArticleDto toDto(Article entity);
+    Article toEntity(ArticleDto dto);
 }
