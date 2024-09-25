36ca1aa4121ce89c2d51d5f30296dc29bd1a9c97
101 <tmddnjs001133@dsm.hs.kr>
2024-09-25 23:36:41 +0900

    add: article repository interface

--- src/main/java/com/practice/board/article/domain/ArticleRepository.java
+++ src/main/java/com/practice/board/article/domain/ArticleRepository.java
@@ -1,2 +1,7 @@
-package com.practice.board.article.domain;interface ArticleRepository {
+package com.practice.board.article.domain;
+
+public interface ArticleRepository {
+    ArticleDto getById(Long id);
+    ArticleDto save(ArticleDto article);
+    void deleteById(Long id);
 }
