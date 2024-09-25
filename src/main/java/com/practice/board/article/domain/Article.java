a01fa225c886d60d531c09e9fae98e1b664791a1
101 <tmddnjs001133@dsm.hs.kr>
2024-09-25 23:36:02 +0900

    add: article entity class

--- src/main/java/com/practice/board/article/domain/Article.java
+++ src/main/java/com/practice/board/article/domain/Article.java
@@ -1,2 +1,26 @@
-package com.practice.board.article.domain;class Article {
+package com.practice.board.article.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+class Article {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 10)
+    private String title;
+
+    @Column(length = 50)
+    private String content;
+
 }
