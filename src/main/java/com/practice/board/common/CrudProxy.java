7dca246cea2912ea23fb4ee28632b690d8f804e4
101 <tmddnjs001133@dsm.hs.kr>
2024-09-26 14:05:59 +0900

    refactor: 상속 관계 형성

--- src/main/java/com/practice/board/common/CrudProxy.java
+++ src/main/java/com/practice/board/common/CrudProxy.java
@@ -1,9 +1,8 @@
 package com.practice.board.common;
 
-public interface CrudProxy<V> {
+public interface CrudProxy<V> extends Proxy<V> {
     V create(Object... args);
     V read(Object... args);
     V update(Object... args);
     void delete(Object... args);
-    void addProxy(CrudProxy<V> crudProxy);
 }
