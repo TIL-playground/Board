ff998f0c18fc4c03067e0679b60909ce8dbaf2d8
101 <tmddnjs001133@dsm.hs.kr>
2024-09-26 14:07:30 +0900

    refactor: 접근 제어자 범위 축소

--- src/main/java/com/practice/board/article/domain/ArticleRepository.java
+++ src/main/java/com/practice/board/article/domain/ArticleRepository.java
@@ -1,6 +1,6 @@
 package com.practice.board.article.domain;
 
-public interface ArticleRepository {
+interface ArticleRepository {
     ArticleDto getById(Long id);
     ArticleDto save(ArticleDto article);
     void deleteById(Long id);
