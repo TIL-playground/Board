364d61614bcae78c9d864895230873e01dea76ac
101 <tmddnjs001133@dsm.hs.kr>
2024-09-26 14:08:12 +0900

    add: article data factory

---
+++ src/main/java/com/practice/board/article/domain/ArticleDataFactory.java
@@ -0,0 +1,20 @@
+package com.practice.board.article.domain;
+
+import com.practice.board.common.DataProxy;
+import com.practice.board.common.Factory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ArticleDataFactory implements Factory<ArticleDto> {
+    private final DataProxy<ArticleDto> dataProxy;
+
+    public ArticleDataFactory(final ArticleDataProxy dataProxy, final ArticleDataLongProxy dataLongProxy) {
+        dataProxy.addProxy(dataLongProxy);
+        this.dataProxy = dataProxy;
+    }
+
+    @Override
+    public DataProxy<ArticleDto> get() {
+        return dataProxy;
+    }
+}
