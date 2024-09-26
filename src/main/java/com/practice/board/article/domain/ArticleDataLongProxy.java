a5a27c3836f6af1083753f2cbe1c1997f02c6222
101 <tmddnjs001133@dsm.hs.kr>
2024-09-26 21:33:40 +0900

    refactor: 변경된 구조 적용

--- src/main/java/com/practice/board/article/domain/ArticleDataLongProxy.java
+++ src/main/java/com/practice/board/article/domain/ArticleDataLongProxy.java
@@ -1,21 +1,19 @@
 package com.practice.board.article.domain;
 
 import com.practice.board.common.DataProxy;
-import com.practice.board.common.Proxy;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 @Component
 @RequiredArgsConstructor
-class ArticleDataLongProxy implements DataProxy<ArticleDto> {
+class ArticleDataLongProxy extends DataProxy<ArticleDto> {
     private final ArticleRepository repository;
-    private DataProxy<ArticleDto> nextProxy;
 
     @Override
     @Transactional
     public ArticleDto save(final Object... args) {
-        return nextProxy != null ? nextProxy.save(args) : null;
+        return proceedSave(args);
     }
 
     @Override
@@ -25,7 +23,7 @@ class ArticleDataLongProxy implements DataProxy<ArticleDto> {
             return repository.getById((Long) args[0]);
         }
 
-        return null;
+        return proceedGet(args);
     }
 
     @Override
@@ -34,17 +32,5 @@ class ArticleDataLongProxy implements DataProxy<ArticleDto> {
         if (args.length == 1 && args[0] instanceof Long) {
             repository.deleteById((Long) args[0]);
         }
-    }
-
-    @Override
-    public void addProxy(final Proxy<ArticleDto> proxy) {
-        assert proxy != null;
-
-        if (nextProxy == null) {
-            if (proxy instanceof DataProxy) {
-                nextProxy = (DataProxy<ArticleDto>) proxy;
-            }
-        }
-        else nextProxy.addProxy(proxy);
     }
 }
--- src/main/java/com/practice/board/article/domain/ArticleDataProxy.java
+++ src/main/java/com/practice/board/article/domain/ArticleDataProxy.java
@@ -1,16 +1,14 @@
 package com.practice.board.article.domain;
 
 import com.practice.board.common.DataProxy;
-import com.practice.board.common.Proxy;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 @Component
 @RequiredArgsConstructor
-class ArticleDataProxy implements DataProxy<ArticleDto> {
+class ArticleDataProxy extends DataProxy<ArticleDto> {
     private final ArticleRepository repository;
-    private DataProxy<ArticleDto> nextProxy;
 
     @Override
     @Transactional
@@ -19,30 +17,18 @@ class ArticleDataProxy implements DataProxy<ArticleDto> {
             return repository.save((ArticleDto) args[0]);
         }
 
-        return null;
+        return proceedSave(args);
     }
 
     @Override
     @Transactional(readOnly = true)
     public ArticleDto get(final Object... args) {
-        return nextProxy != null ? nextProxy.get(args) : null;
+        return proceedGet(args);
     }
 
     @Override
     @Transactional
     public void delete(final Object... args) {
-        if (nextProxy != null) nextProxy.delete(args);
-    }
-
-    @Override
-    public void addProxy(final Proxy<ArticleDto> proxy) {
-        assert proxy != null;
-
-        if (nextProxy == null) {
-            if (proxy instanceof DataProxy) {
-                nextProxy = (DataProxy<ArticleDto>) proxy;
-            }
-        }
-        else nextProxy.addProxy(proxy);
+        proceedDelete(args);
     }
 }
