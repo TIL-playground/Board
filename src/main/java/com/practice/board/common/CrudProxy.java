3b7db713680e7f137f74a43338a7fee41b1864d4
101 <tmddnjs001133@dsm.hs.kr>
2024-09-26 21:24:29 +0900

    refactor: proxy 구조 변경

--- src/main/java/com/practice/board/common/CrudProxy.java
+++ src/main/java/com/practice/board/common/CrudProxy.java
@@ -1,8 +1,39 @@
 package com.practice.board.common;
 
-public interface CrudProxy<V> extends Proxy<V> {
-    V create(Object... args);
-    V read(Object... args);
-    V update(Object... args);
-    void delete(Object... args);
+public abstract class CrudProxy<V> implements Proxy<V> {
+    protected CrudProxy<V> next;
+
+    public abstract V create(Object... args);
+    public abstract V read(Object... args);
+    public abstract V update(Object... args);
+    public abstract void delete(Object... args);
+
+    public void setNext(CrudProxy<V> next) {
+        this.next = next;
+    }
+
+    protected V proceedCreate(Object... args) {
+        return next != null ? next.create(args) : null;
+    }
+
+    protected V proceedRead(Object... args) {
+        return next != null ? next.read(args) : null;
+    }
+
+    protected V proceedUpdate(Object... args) {
+        return next != null ? next.update(args) : null;
+    }
+
+    protected void proceedDelete(Object... args) {
+        if (next != null) next.delete(args);
+    }
+
+    @Override
+    public void addProxy(Proxy<V> next) {
+        if (this.next == null && next instanceof CrudProxy) this.next = (CrudProxy<V>) next;
+        else {
+            assert this.next != null;
+            this.next.addProxy(next);
+        }
+    }
 }
