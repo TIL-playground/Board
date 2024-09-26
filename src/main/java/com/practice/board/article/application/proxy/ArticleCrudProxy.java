ef4e1ba37068000cfa8934bed975db082aef2caf
101 <tmddnjs001133@dsm.hs.kr>
2024-09-26 14:06:54 +0900

    feat: add proxy 프로세스 변경 및 트랜잭션 범위 축소

--- src/main/java/com/practice/board/article/application/proxy/ArticleCrudProxy.java
+++ src/main/java/com/practice/board/article/application/proxy/ArticleCrudProxy.java
@@ -3,18 +3,17 @@ package com.practice.board.article.application.proxy;
 import com.practice.board.article.application.service.ArticleService;
 import com.practice.board.article.domain.ArticleDto;
 import com.practice.board.common.CrudProxy;
+import com.practice.board.common.Proxy;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 
 @Component
 @RequiredArgsConstructor
-public class ArticleCrudProxy implements CrudProxy<ArticleDto> {
+class ArticleCrudProxy implements CrudProxy<ArticleDto> {
     private CrudProxy<ArticleDto> nextProxy;
     private final ArticleService service;
 
     @Override
-    @Transactional
     public ArticleDto create(final Object... args) {
         if (args.length == 1 && args[0] instanceof ArticleDto) {
             return service.save((ArticleDto) args[0]);
@@ -24,13 +23,11 @@ public class ArticleCrudProxy implements CrudProxy<ArticleDto> {
     }
 
     @Override
-    @Transactional(readOnly = true)
     public ArticleDto read(final Object... args) {
         return nextProxy != null ? nextProxy.read(args) : null;
     }
 
     @Override
-    @Transactional
     public ArticleDto update(final Object... args) {
         if (args.length == 1 && args[0] instanceof ArticleDto) {
             return service.save((ArticleDto) args[0]);
@@ -45,8 +42,14 @@ public class ArticleCrudProxy implements CrudProxy<ArticleDto> {
     }
 
     @Override
-    public void addProxy(final CrudProxy<ArticleDto> crudProxy) {
-        if (nextProxy == null) nextProxy = crudProxy;
-        else nextProxy.addProxy(crudProxy);
+    public void addProxy(final Proxy<ArticleDto> proxy) {
+        assert proxy != null;
+
+        if (nextProxy == null) {
+            if (proxy instanceof CrudProxy) {
+                nextProxy = (CrudProxy<ArticleDto>) proxy;
+            }
+        }
+        else nextProxy.addProxy(proxy);
     }
 }
