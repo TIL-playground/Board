c8d5235551a66bdaabb5640ee87cf20ca94d60fb
101 <tmddnjs001133@dsm.hs.kr>
2024-10-07 12:10:01 +0900

    refactor: static import 사용

--- board-api/article-api/src/main/java/module/board/interfaces/ArticleHttpBodyMapper.java
+++ board-api/article-api/src/main/java/module/board/interfaces/ArticleHttpBodyMapper.java
@@ -2,10 +2,11 @@ package module.board.interfaces;
 
 import module.board.application.ArticleDto;
 import org.mapstruct.Mapper;
-import org.mapstruct.MappingConstants;
-import org.mapstruct.ReportingPolicy;
 
-@Mapper(componentModel = MappingConstants.ComponentModel.SPRING, unmappedTargetPolicy = ReportingPolicy.IGNORE)
+import static org.mapstruct.MappingConstants.ComponentModel.SPRING;
+import static org.mapstruct.ReportingPolicy.IGNORE;
+
+@Mapper(componentModel = SPRING, unmappedTargetPolicy = IGNORE)
 interface ArticleHttpBodyMapper {
     ArticleDto toDto(ArticleRequestDto dto);
     ArticleResponseDto toResponse(ArticleDto dto);
--- board-domain/article-domain/src/main/java/module/board/domain/ArticleDomainMapper.java
+++ board-domain/article-domain/src/main/java/module/board/domain/ArticleDomainMapper.java
@@ -1,10 +1,11 @@
 package module.board.domain;
 
 import org.mapstruct.Mapper;
-import org.mapstruct.MappingConstants;
-import org.mapstruct.ReportingPolicy;
 
-@Mapper(componentModel = MappingConstants.ComponentModel.SPRING, unmappedTargetPolicy = ReportingPolicy.IGNORE)
+import static org.mapstruct.ReportingPolicy.IGNORE;
+import static org.mapstruct.MappingConstants.ComponentModel.SPRING;
+
+@Mapper(componentModel = SPRING, unmappedTargetPolicy = IGNORE)
 interface ArticleDomainMapper {
     ArticleDto toDto(Article entity);
     Article toEntity(ArticleDto dto);
