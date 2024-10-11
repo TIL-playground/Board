221540c5a66afd814dd8fd77791489a73cc8b5bc
101 <tmddnjs001133@dsm.hs.kr>
2024-10-11 19:14:42 +0900

    add: user data factory

---
+++ board-domain/user-domain/src/main/java/module/board/domain/UserDataFactory.java
@@ -0,0 +1,22 @@
+package module.board.domain;
+
+import module.board.common.DataProxy;
+import module.board.common.Factory;
+import module.board.common.Proxy;
+import org.springframework.stereotype.Component;
+
+@Component
+class UserDataFactory implements Factory<UserDto> {
+    private final DataProxy<UserDto> dataProxy;
+
+    private UserDataFactory(final UserDataProxy dataProxy, final UserDataLongProxy longProxy, final UserDataDtoProxy dtoProxy) {
+        dataProxy.addProxy(longProxy);
+        dataProxy.addProxy(dtoProxy);
+        this.dataProxy = dataProxy;
+    }
+
+    @Override
+    public Proxy<UserDto> get() {
+        return dataProxy;
+    }
+}
