65a7df0c8caa4e9e0a8c7337ac264a8aa75ac1ef
101 <tmddnjs001133@dsm.hs.kr>
2024-10-11 19:14:30 +0900

    add: user data dto proxy

---
+++ board-domain/user-domain/src/main/java/module/board/domain/UserDataDtoProxy.java
@@ -0,0 +1,30 @@
+package module.board.domain;
+
+import lombok.RequiredArgsConstructor;
+import module.board.common.DataProxy;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+class UserDataDtoProxy extends DataProxy<UserDto> {
+    private final UserRepository repository;
+
+    @Override
+    public UserDto save(final Object... args) {
+        if (args.length == 1 && args[0] instanceof UserDto) {
+            return repository.save((UserDto) args[0]);
+        }
+
+        return proceedSave(args);
+    }
+
+    @Override
+    public Object get(final Object... args) {
+        return proceedGet(args);
+    }
+
+    @Override
+    public void delete(final Object... args) {
+        proceedDelete(args);
+    }
+}
