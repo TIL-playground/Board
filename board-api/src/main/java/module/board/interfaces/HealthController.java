a2476eee36a21163c92019319340ac0c8c4741f8
101 <tmddnjs001133@dsm.hs.kr>
2024-10-11 13:04:17 +0900

    add: server health check controller

---
+++ board-api/src/main/java/module/board/interfaces/HealthController.java
@@ -0,0 +1,14 @@
+package module.board.interfaces;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    @GetMapping("/health")
+    public ResponseEntity<Void> health() {
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+}
