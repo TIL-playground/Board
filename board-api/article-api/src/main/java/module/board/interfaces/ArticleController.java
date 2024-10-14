2d54394d20875d6309f9edb37f5ebc9fe8ecf633
101 <tmddnjs001133@dsm.hs.kr>
2024-10-14 16:44:48 +0900

    update: use ApiResponse

--- board-api/article-api/src/main/java/module/board/interfaces/ArticleController.java
+++ board-api/article-api/src/main/java/module/board/interfaces/ArticleController.java
@@ -1,14 +1,16 @@
 package module.board.interfaces;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import module.board.application.ArticleApplicationProxyHandler;
 import module.board.application.ArticleDto;
 import module.board.common.hash.HashUtil;
+import module.board.common.http.HttpStatusUtil;
 import module.board.common.request.PageRequest;
+import module.board.common.response.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Collections;
 import java.util.List;
 
 @RestController
@@ -20,51 +22,72 @@ class ArticleController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    void createArticle(@RequestBody final ArticleRequestDto httpBody) {
+    ApiResponse<Void> createArticle(@RequestBody final ArticleRequestDto httpBody, HttpServletRequest request) {
         final var dto = mapper.toDto(httpBody);
         handler.create(dto);
+
+        return ApiResponse.success(
+                HttpStatusUtil.CREATED,
+                request.getRequestURI()
+        );
     }
 
     @GetMapping("/{id}")
-    ArticleResponseDto getArticle(@PathVariable final String id) {
+    ApiResponse<ArticleResponseDto> getArticle(@PathVariable final String id, HttpServletRequest request) {
         final long decodedId = HashUtil.decode(id);
         final var result = (ArticleDto) handler.read(decodedId);
 
-        return ArticleResponseDto.fromArticleDto(result);
+        return ApiResponse.success(
+                HttpStatusUtil.OK,
+                ArticleResponseDto.fromArticleDto(result),
+                request.getRequestURI()
+        );
     }
 
     @GetMapping
-    List<ArticleResponseDto> getAll(
+    ApiResponse<List<ArticleResponseDto>> getAll(
             @RequestParam(defaultValue = "0") final int page,
-            @RequestParam(defaultValue = "10") final int size
+            @RequestParam(defaultValue = "10") final int size,
+            HttpServletRequest request
     ) {
         final var result = (List<ArticleDto>) handler.read(new PageRequest(page, size));
 
-        if (result == null) {
-            return Collections.emptyList();
-        }
-
-        return result.stream()
-                .map(ArticleResponseDto::fromArticleDto)
-                .toList();
+        return ApiResponse.success(
+                HttpStatusUtil.OK,
+                result.stream()
+                        .map(ArticleResponseDto::fromArticleDto)
+                        .toList(),
+                request.getRequestURI()
+        );
     }
 
     @PatchMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    void updateArticle(
+    ApiResponse<Void> updateArticle(
             @PathVariable final String id,
-            @RequestBody final ArticleRequestDto httpBody
+            @RequestBody final ArticleRequestDto httpBody,
+            HttpServletRequest request
     ) {
         final long decodedId = HashUtil.decode(id);
         final var dto = mapper.toDto(httpBody.addId(decodedId));
 
         handler.update(dto);
+
+        return ApiResponse.success(
+                HttpStatusUtil.NO_CONTENT,
+                request.getRequestURI()
+        );
     }
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    void deleteArticle(@PathVariable final String id) {
+    ApiResponse<Void> deleteArticle(@PathVariable final String id, HttpServletRequest request) {
         final long decodedId = HashUtil.decode(id);
         handler.delete(decodedId);
+
+        return ApiResponse.success(
+                HttpStatusUtil.NO_CONTENT,
+                request.getRequestURI()
+        );
     }
 }
--- board-api/src/main/java/module/board/exception/BaseControllerAdvice.java
+++ board-api/src/main/java/module/board/exception/BaseControllerAdvice.java
@@ -1,11 +1,12 @@
 package module.board.exception;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
-import module.board.common.error.ErrorResponse;
 import module.board.common.exception.BadRequestException;
 import module.board.common.exception.DataNotFoundException;
 import module.board.common.exception.DuplicateException;
 import module.board.common.exception.ServerException;
+import module.board.common.response.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -14,58 +15,78 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
 import java.util.HashMap;
 import java.util.Map;
 
 @Slf4j
 @RestControllerAdvice
 public class BaseControllerAdvice {
-    public static final Instant TIMESTAMP = LocalDateTime.now().atZone(ZoneId.systemDefault()).toInstant();
 
     @ExceptionHandler(NoHandlerFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ErrorResponse noHandlerFoundException(final NoHandlerFoundException ex) {
+    public ApiResponse<Void> noHandlerFoundException(final NoHandlerFoundException ex, final HttpServletRequest request) {
         log.debug(ex.getMessage(), ex.getCause());
-        return ErrorResponse.newResponse(HttpStatus.NOT_FOUND.value(), "존재하지 않는 API 엔드포인트로 요청을 보내셨습니다. 요청 내용을 다시 확인해주세요.", TIMESTAMP);
+        return ApiResponse.error(
+                HttpStatus.NOT_FOUND.value(),
+                "존재하지 않는 API 엔드포인트로 요청을 보내셨습니다. 요청 내용을 다시 확인해주세요.",
+                request.getRequestURI()
+        );
     }
 
     @ExceptionHandler(DataNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ErrorResponse noHandlerFoundException(final Exception ex) {
-        return ErrorResponse.newResponse(HttpStatus.NOT_FOUND.value(), ex.getMessage(), TIMESTAMP);
+    public ApiResponse<Void> noHandlerFoundException(final Exception ex, final HttpServletRequest request) {
+        return ApiResponse.error(
+                HttpStatus.NOT_FOUND.value(),
+                ex.getMessage(),
+                request.getRequestURI()
+        );
     }
 
     @ExceptionHandler({BadRequestException.class, DuplicateException.class})
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResponse handleBadRequestException(final Exception ex) {
-        return ErrorResponse.newResponse(HttpStatus.BAD_REQUEST.value(), ex.getMessage(), TIMESTAMP);
+    public ApiResponse<Void> handleBadRequestException(final Exception ex, final HttpServletRequest request) {
+        return ApiResponse.error(
+                HttpStatus.BAD_REQUEST.value(),
+                ex.getMessage(),
+                request.getRequestURI()
+        );
     }
 
     @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
     @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
-    public ErrorResponse notSupportedException(final HttpRequestMethodNotSupportedException ex) {
+    public ApiResponse<Void> notSupportedException(final HttpRequestMethodNotSupportedException ex, final HttpServletRequest request) {
         log.debug(ex.getMessage(), ex.getCause());
-        return ErrorResponse.newResponse(HttpStatus.METHOD_NOT_ALLOWED.value(),"허용되지 않은 메서드로 요처을 하셨습니다. 요청 내용을 다시 확인해주세요.", TIMESTAMP);
+        return ApiResponse.error(
+                HttpStatus.METHOD_NOT_ALLOWED.value(),
+                "허용되지 않은 메서드로 요처을 하셨습니다. 요청 내용을 다시 확인해주세요.",
+                request.getRequestURI()
+        );
     }
 
     @ExceptionHandler({Exception.class, ServerException.class})
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ErrorResponse handleAllExceptions(final Exception ex) {
+    public ApiResponse<Void> handleAllExceptions(final Exception ex, final HttpServletRequest request) {
         log.error(ex.getMessage(), ex.getLocalizedMessage());
-        return ErrorResponse.newResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), ex.getMessage(), TIMESTAMP);
+        return ApiResponse.error(
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                ex.getMessage(),
+                request.getRequestURI()
+        );
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ErrorResponse handleValidationExceptionHandler(final MethodArgumentNotValidException ex) {
+    public ApiResponse<Void> handleValidationExceptionHandler(final MethodArgumentNotValidException ex, final HttpServletRequest request) {
         Map<String, String> errors = new HashMap<>();
 
         ex.getBindingResult().getFieldErrors().forEach(error ->
                 errors.put(error.getField(), error.getDefaultMessage()));
 
-        return ErrorResponse.newResponse(HttpStatus.BAD_REQUEST.value(), errors.toString(), TIMESTAMP);
+        return ApiResponse.error(
+                HttpStatus.BAD_REQUEST.value(),
+                errors.toString(),
+                request.getRequestURI()
+        );
     }
 }
