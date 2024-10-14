d400284cff174833970085af59c75442ba5c7ad6
101 <tmddnjs001133@dsm.hs.kr>
2024-10-14 16:42:56 +0900

    setting: gradle dependency

--- board-security/build.gradle.kts
+++ board-security/build.gradle.kts
@@ -1,4 +1,5 @@
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("dev.paseto:jpaseto-its:0.7.0")
+    implementation(project(":board-common"))
 }
